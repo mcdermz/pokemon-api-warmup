852ba0b108c31ff480d632e57500a60fd09e6d27
Sean McDermott <mcderm.sean@gmail.com>
2017-06-01 10:37:22 -0700

    refactored loading div and capitalized pokemon name

--- main.js
+++ main.js
@@ -6,21 +6,25 @@
       controller: controller,
       templateUrl: './main.html',
     })
+
     controller.$inject = ['$http']
+
+    function capitalize(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
     function controller ($http){
       const vm = this
-      const randomPokemon = []
-      vm.$onInit = function (){}
 
       vm.generatePokemon = function (){
         vm.isLoading = true
         const pokeId = Math.ceil(Math.random() * 722)
-        console.log('generating...');
+
         $http.get(`http://pokeapi.co/api/v2/pokemon/${pokeId}`)
         .then(response => {
           vm.pokemon = {
-            name: response.data.name,
-            front_image: response.data.sprites.front_shiny,
+            name: capitalize(response.data.name),
+            front_image: response.data.sprites.front_default,
           }
           vm.isLoading = false
         })
